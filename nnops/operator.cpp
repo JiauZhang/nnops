f1e8e35d218028ab775fd12b3a3577d0d80c253a
JiauZhang <1743960454@qq.com>
2024-11-16 16:39:41 +0800

    add operator

---
+++ nnops/operator.cpp
@@ -0,0 +1,36 @@
+#include <nnops/operator.h>
+
+namespace nnops {
+
+std::map<std::string, Operator *> Operator::operators_[DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES];
+
+void Operator::register_operator(std::string &op_name, DeviceType type, Operator *op) {
+    auto &ops_ = Operator::operators_[type];
+    if (ops_.count(op_name)) {
+        Device *device = Device::get_device(type);
+        std::string info = "Operator <" + op_name + "> has been registered in "
+            + device->get_device_name() + " device!";
+        throw std::runtime_error(info);
+    }
+    ops_[op_name] = op;
+}
+
+Operator *Operator::get_operator(std::string &op_name, DeviceType type) {
+    auto &ops_ = Operator::operators_[type];
+    if (ops_.count(op_name))
+        return ops_[op_name];
+    else
+        return nullptr;
+}
+
+void Operator::set_operator_name(std::string &op_name) {
+    DeviceType type = get_device_type();
+    auto &ops_ = Operator::operators_[type];
+    auto iter = ops_.find(op_name);
+    if (iter != ops_.end())
+        operator_name_ = iter->first.c_str();
+    else
+        throw std::runtime_error("set_operator_name failed!");
+}
+
+} // namespace nnops
--- nnops/operator.h
+++ nnops/operator.h
@@ -10,10 +10,20 @@ namespace nnops {
 
 class Operator {
 public:
+    void set_operator_name(std::string &op_name);
+    inline std::string get_operator_name() { return std::string(operator_name_); }
+    inline void set_device_type(DeviceType &dt) { device_type_ = dt; }
+    inline DeviceType get_device_type() { return device_type_; }
+
     static void register_operator(std::string &op_name, DeviceType type, Operator *op);
     static Operator *get_operator(std::string &op_name, DeviceType type);
 
-    virtual void operator() = 0;
+    virtual void operator()(void) = 0;
+
+private:
+    static std::map<std::string, Operator *> operators_[DeviceType::COMPILE_TIME_MAX_DEVICE_TYPES];
+    const char *operator_name_ = nullptr;
+    DeviceType device_type_;
 };
 
 } // namespace nnops
