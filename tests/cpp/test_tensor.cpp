736f4e95ce223135c06bdff6c3cc9dd0aa291f9c
JiauZhang <1743960454@qq.com>
2025-01-04 18:27:27 +0800

    EXPECT_EQ to ASSERT_EQ

--- tests/cpp/test_tensor.cpp
+++ tests/cpp/test_tensor.cpp
@@ -25,31 +25,31 @@ protected:
 
 TEST_F(TensorTest, TensorInit) {
     Tensor t_cpu_u8(DataType::TYPE_UINT8, shape, DeviceType::CPU);
-    EXPECT_EQ(t_cpu_u8.nelems(), 360);
-    EXPECT_EQ(t_cpu_u8.nbytes(), 360);
+    ASSERT_EQ(t_cpu_u8.nelems(), 360);
+    ASSERT_EQ(t_cpu_u8.nbytes(), 360);
     Tensor t_cpu_f32(DataType::TYPE_FLOAT32, shape, DeviceType::CPU);
-    EXPECT_EQ(t_cpu_f32.nelems(), 360);
-    EXPECT_EQ(t_cpu_f32.nbytes(), 360 * 4);
+    ASSERT_EQ(t_cpu_f32.nelems(), 360);
+    ASSERT_EQ(t_cpu_f32.nbytes(), 360 * 4);
 }
 
 TEST_F(TensorTest, UnravelIndex) {
-    EXPECT_EQ(shape.size() * indices.size(), outputs.size());
+    ASSERT_EQ(shape.size() * indices.size(), outputs.size());
     for (int i = 0; i < indices.size(); i++) {
         TensorShape out = Tensor::unravel_index(indices[i], shape);
-        EXPECT_EQ(out.size(), shape.size());
+        ASSERT_EQ(out.size(), shape.size());
         for (int j = 0; j < shape.size(); j++)
-            EXPECT_EQ(out[j], outputs[i * shape.size() + j]);
+            ASSERT_EQ(out[j], outputs[i * shape.size() + j]);
     }
 }
 
 TEST_F(TensorTest, RavelIndex) {
-    EXPECT_EQ(shape.size() * indices.size(), outputs.size());
+    ASSERT_EQ(shape.size() * indices.size(), outputs.size());
     for (int i = 0; i < indices.size(); i++) {
         TensorShape dims(shape.size());
         for (int j = 0; j < shape.size(); j++)
             dims[j] = outputs[i * shape.size() + j];
         index_t out = Tensor::ravel_index(dims, shape);
-        EXPECT_EQ(out, indices[i]);
+        ASSERT_EQ(out, indices[i]);
     }
 }
 
@@ -64,24 +64,24 @@ TEST_F(TensorTest, TensorIteratorAndAccessor) {
     TensorIterator iter = t.begin(), iter_end = t.end();
     i = 0;
     for (; iter != iter_end; ++iter, ++i)
-        EXPECT_EQ(*iter, (void *)(ptr + i));
+        ASSERT_EQ(*iter, (void *)(ptr + i));
 
     TensorAccessor acc = t.accessor();
     TensorShape anchor_0 = {0, 0, 0}, anchor_1 = {1, 1, 2}, anchor_2 = {0, 1, 1};
     float *ptr_0 = (float *)acc.data_ptr_unsafe(anchor_0);
-    EXPECT_EQ(ptr_0, ptr);
+    ASSERT_EQ(ptr_0, ptr);
     float *ptr_1 = (float *)acc.data_ptr_unsafe(anchor_1);
-    EXPECT_EQ(ptr_1, ptr + 11);
+    ASSERT_EQ(ptr_1, ptr + 11);
     float *ptr_2 = (float *)acc.data_ptr_unsafe(anchor_2);
-    EXPECT_EQ(ptr_2, ptr + 4);
+    ASSERT_EQ(ptr_2, ptr + 4);
 
     for (i = 0; i < s[1]; i++) {
         for (int j = 0; j < s[2]; j++) {
             TensorShape anchor_ij = {0, i, j};
             float *ptr_ij = (float *)acc.data_ptr_unsafe(anchor_ij);
-            EXPECT_EQ(*ptr_ij, *(ptr + i * 3 + j));
+            ASSERT_EQ(*ptr_ij, *(ptr + i * 3 + j));
             float *ptr_ij_next = (float *)acc.data_ptr_unsafe((void *)ptr_ij, 1, 0);
-            EXPECT_EQ(*ptr_ij_next, *(ptr + 6 + i * 3 + j));
+            ASSERT_EQ(*ptr_ij_next, *(ptr + 6 + i * 3 + j));
         }
     }
 }
